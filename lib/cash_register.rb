306e938d9c77677cd5aa98b5271722cea24ef5bd
+ <+>
2022-10-07 09:17:52 -0400

    commit tree ecf4603e100d7c30cbfd1c1135d2d05155b52b40

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
 
+require 'pry'
+
+class CashRegister
+    
+    attr_accessor :cash_register_with_discount, :total, :add_item, :apply_discount, :items, :void_last_transaction
+
+    def initialize(cash_register_with_discount=0)
+        @cash_register_with_discount = cash_register_with_discount
+        self.total = 0
+    end
+
+    def total
+    end
+
+    def add_item
+    end
+
+    def apply_discount
+    end
+
+    def items
+    end
+
+    def void_last_transaction
+    end
+
+end
+
+binding.pry
